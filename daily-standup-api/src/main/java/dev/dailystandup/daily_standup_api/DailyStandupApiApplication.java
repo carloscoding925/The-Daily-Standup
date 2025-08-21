115f05ff3e0fbf57e45e6dcf3dc9b2a3b0b27d9f
Carlos Hernandez <carlosandresh925@gmail.com>
2025-08-20 19:33:31 -0700

    Add api

---
+++ daily-standup-api/src/main/java/dev/dailystandup/daily_standup_api/DailyStandupApiApplication.java
@@ -0,0 +1,13 @@
+package dev.dailystandup.daily_standup_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyStandupApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyStandupApiApplication.class, args);
+	}
+
+}
---
+++ daily-standup-api/src/test/java/dev/dailystandup/daily_standup_api/DailyStandupApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dailystandup.daily_standup_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyStandupApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
